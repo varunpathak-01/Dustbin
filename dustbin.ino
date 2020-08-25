c417babce28b65ed519cda33854c1178eb991ff5
varunpathak-01 <65180785+varunpathak-01@users.noreply.github.com>
2020-08-25 20:45:17 +0530

    Update dustbin.ino

--- dustbin.ino
+++ dustbin.ino
@@ -1,29 +1,38 @@
-// include the library code:
 #include <LiquidCrystal.h>
+LiquidCrystal lcd(12, 11, 5, 4, 3, 2);
 const int trig=9;
 const int echo=10;
-// initialize the library with the numbers of the interface pins
-LiquidCrystal lcd(12, 11, 5, 4, 3, 2);
-
-void setup() {
-   Serial.begin(9600);
+void setup()
+{ Serial.begin(9600);
   pinMode(trig,OUTPUT);
   pinMode(echo,INPUT);
-pinMode(8,OUTPUT); 
+  pinMode(11,OUTPUT);
+  pinMode(8,OUTPUT);
 }
-
 void loop() {
   digitalWrite(trig,HIGH);
-  digitalWrite(8,HIGH);
+ digitalWrite(8,HIGH);
 delayMicroseconds(10);
 digitalWrite(trig,LOW);
 delayMicroseconds(10);
 long x=pulseIn(echo,HIGH);
 int d=x/29/2;
+int i,y;
 Serial.println(d);
+if(d<=5)
+{lcd.begin(16, 2);
+  lcd.print("Dustbin Full!!");
+  delay(300);
+ }
+ else
+ {i=21-d;
+ //y=(i/21)*100;
  lcd.begin(16, 2);
-  lcd.print("water level-");
- lcd.print(d);
- delay(1000);
+   lcd.print("Storage-");
+   lcd.print(i);
+   lcd.print("cm");
+   
+  
+   delay(300);
+ }
 }
-
